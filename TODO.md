04b19754aedb14cfa026c5f015f8894ec8f70d7d
alexjpujol <apujol@spotify.com>
2018-03-28 15:56:24 -0400

    logos added

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# LIST OF TODOS
+1. Add logos for leagues
+    * download logos and move into static assets folder
+2. Work on players page
+3. Work on teams page
+4. Make components reusable
--- app/components/competitions/CompetitionsGrid.js
+++ app/components/competitions/CompetitionsGrid.js
@@ -6,40 +6,50 @@ import Api from '../../utils/api';
 
 import LeagueTable from './LeagueTable';
 
+const leagueLogos = {
+        "SA": "https://upload.wikimedia.org/wikipedia/en/f/f7/LegaSerieAlogoTIM.png",
+        "BSA": "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Primeira_Liga_%28Brazil%29.png/250px-Primeira_Liga_%28Brazil%29.png",
+        "PL": "https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Premier_League_Logo.svg/1200px-Premier_League_Logo.svg.png",
+        "ELC": "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/EFL_Championship.svg/1200px-EFL_Championship.svg.png",
+        "EL1": "https://upload.wikimedia.org/wikipedia/en/thumb/c/cb/EFL_League_One.svg/1200px-EFL_League_One.svg.png",
+        "EL2": "https://vignette.wikia.nocookie.net/the-football-database/images/c/cc/Football_League_Two_Logo.png/revision/latest?cb=20140811133847",
+        "DED": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eredivisie_nieuw_logo_2017-.svg/1200px-Eredivisie_nieuw_logo_2017-.svg.png",
+        "FL1": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Ligue1.svg/2000px-Ligue1.svg.png",
+        "FL2": "https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/Ligue_2_logo.svg/1200px-Ligue_2_logo.svg.png",
+        "BL1": "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/Bundesliga_logo_%282017%29.svg/1200px-Bundesliga_logo_%282017%29.svg.png",
+        "BL2": "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/2._Bundesliga_logo.svg/1200px-2._Bundesliga_logo.svg.png",
+        "PD": "https://upload.wikimedia.org/wikipedia/commons/0/04/La_Liga_logotip.jpg",
+        "PPL": "https://upload.wikimedia.org/wikipedia/commons/0/0e/Liga_NOS_logo.png",
+        "DFB": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/DFB-Pokal_logo_2016.svg/1200px-DFB-Pokal_logo_2016.svg.png",
+        "SB": "https://upload.wikimedia.org/wikipedia/it/7/7e/Logo_Serie_B_2015.png",
+        "CL": "https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/UEFA_Champions_League_logo_2.svg/1200px-UEFA_Champions_League_logo_2.svg.png",
+        "AAL": "https://upload.wikimedia.org/wikipedia/en/thumb/8/85/Hyundai_A-League_logo_%282017%E2%80%93%29.svg/1200px-Hyundai_A-League_logo_%282017%E2%80%93%29.svg.png"
+}
 
-class CompetitionsGrid extends React.Component {
 
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            table: []
-        }
-    }
+const CompetitionsGrid = props => {
 
-    render() {
-        const match = this.props.url;
+    const match = props.url;
 
-        return(
-            <div className="competitions-grid">
-                {this.props.competitions.map(competition => {
-                    return (
-                        <div className="competition-square" key={competition.id}>
-                            <ul>
-                                <li><span className="league-name">{competition.caption}</span></li>
-                                <li onClick={this.clickLeagueTable}>
-                                    <Link to={{
-                                        pathname: `${match.url}/${competition.id}/leagueTable`
-                                    }}>Table</Link>
-                                </li>
-                                <li teams={competition._links.teams.href}>Teams</li>
-                            </ul>
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    }
+    return(
+        <div className="competitions-grid">
+            {props.competitions.map(competition => {
+                return (
+                    <div className="competition-square" key={competition.id}>
+                        <ul>
+                            <li>
+                                <Link to={{
+                                    pathname: `${match.url}/${competition.id}/leagueTable`
+                                }}><img src={leagueLogos[competition.league]} alt={competition.caption} height="120px"/></Link>
+                            </li>
+                        </ul>
+                    </div>
+                )
+            })}
+        </div>
+    )
+
 }
 
 CompetitionsGrid.propTypes = {
