8a8d48489ec74bb4c5ce95815f525b400a147117
alexjpujol <apujol@spotify.com>
2018-03-28 11:06:49 -0400

    table styling

--- app/components/tables/Table.js
+++ app/components/tables/Table.js
@@ -6,26 +6,28 @@ import Api from '../../utils/api';
 
 const TableHTML = props => {
     return (
-        <table>
-            <tbody>
+        <table className="league-table">
+            <thead>
                 <tr>
                     <th></th>
                     <th>Team</th>
                     <th>Wins</th>
                     <th>Draws</th>
                     <th>Losses</th>
-                    <th>Goal Differential</th>
+                    <th>+ / -</th>
                     <th>Points</th>
                 </tr>
+            </thead>
+            <tbody>
                 {props.table.map(row => {
                     return(
                         <tr key={row.position}>
-                            <td><img src={row.crestURI} width="30px" /></td>
+                            {!row.crestURI ? <td></td> : <td><img src={row.crestURI} width="30px" /></td> }
                             <td>{row.teamName}</td>
-                            <td>{row.wins}</td>
-                            <td>{row.draws}</td>
-                            <td>{row.losses}</td>
-                            <td>{row.goalDifference}</td>
+                            <td className="wins">{row.wins}</td>
+                            <td className="draws">{row.draws}</td>
+                            <td className="losses">{row.losses}</td>
+                            {row.goalDifference > 0 ? <td className="positive">{row.goalDifference}</td> : <td className="negative">{row.goalDifference}</td>}
                             <td>{row.points}</td>
                         </tr>
                     )
@@ -64,7 +66,12 @@ class Table extends React.Component {
     render() {
         return(
             <div>
-                {!this.state.table ? <p className="loading">"Loading"</p> : <TableHTML table={this.state.table.standing}/>}
+                {!this.state.table ? <p className="loading">"Loading"</p> : 
+                    <div>
+                        <h1 className="league-name">{this.state.table.leagueCaption}</h1>
+                        <TableHTML table={this.state.table.standing} />
+                    </div>
+                }
             </div>
         )
     }
