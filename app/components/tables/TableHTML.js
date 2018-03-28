04cb43f928130a598e4051632316fca21485d337
alexjpujol <apujol@spotify.com>
2018-03-28 14:38:17 -0400

    merging branch in

---
+++ app/components/tables/TableHTML.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+import Logo from './Logo';
+
+const TableHTML = props => {
+    return (
+        <table className="league-table">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Team</th>
+                    <th>Wins</th>
+                    <th>Draws</th>
+                    <th>Losses</th>
+                    <th>+ / -</th>
+                    <th>Points</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.table.map(row => {
+                    return(
+                        <tr key={row.position}>
+                            <td><Logo src={row.crestURI} alt={row.teamName} /></td>
+                            <td className="team-name">{row.teamName}</td>
+                            <td className="wins">{row.wins}</td>
+                            <td className="draws">{row.draws}</td>
+                            <td className="losses">{row.losses}</td>
+                            {row.goalDifference > 0 ? <td className="positive">{row.goalDifference}</td> : <td className="negative">{row.goalDifference}</td>}
+                            <td>{row.points}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
+
+TableHTML.propTypes = {
+    table: PropTypes.array.isRequired
+}
+
+module.exports = TableHTML;
