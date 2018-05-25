f1eebb21a19cd4940ab94a6e1e45bafc6210694c
alexjpujol <apujol@spotify.com>
2018-05-25 08:56:40 -0400

    team page working

--- app/components/teams/Teams.js
+++ app/components/teams/Teams.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../common/Logo';
 import teams from '../../../public/json/teams';
 import Api from '../../utils/api';
 
-const Teams = () => {
-    console.log(teams)
+const Teams = props => {
+    console.log(props)
+
+    const match = props.match;
 
     return(
-        <div>
+        <div className="container">
             <h1>Teams</h1>
-                <ul>
+                <ul className="teams">
                     {teams.map(team => {
                         return(
-                            <li className="team">
-
+                            <li className="team" key={team.id}>
+                                <img src={team.logo} height="150px" />
+                                <Link to={{
+                                    pathname: `${match.url}/${team.id}`
+                                }}>
+                                    <h3>{team.name}</h3>
+                                </Link>
                             </li>
                         )
                     })}
