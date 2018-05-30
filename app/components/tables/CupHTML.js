d8485973ad1a5a028d74f62addc2784b30897dcb
alexjpujol <apujol@spotify.com>
2018-05-30 13:26:57 -0400

    cup styling done

--- app/components/tables/CupHTML.js
+++ app/components/tables/CupHTML.js
@@ -14,35 +14,33 @@ const CupHTML = props => {
         groups.push(props.cup[group])
     }
 
-    console.log(groups)
-
-
     return (
-        <div>
+        <div className="cup-tables">
             {groups.map(group => {
-                console.log(group[0])
                 return(
                     <div key={Math.round(Math.random() * 1000000)}>
                         <h2>Group {group[0]["group"]}</h2>
                         <table key={group[0]["teamId"]}>
-                                <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th>Team</th>
-                                        <th>Goal Differential</th>
-                                    </tr>
-                                </thead>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Team</th>
+                                    <th>+ / -</th>
+                                    <th>Points</th>
+                                </tr>
+                            </thead>
+                            <tbody>
                                 {group.map(team => {
                                     return(
-                                        <tbody key={team.teamId}>
-                                            <tr>
-                                                <td><Logo src={team.crestURI} alt={team.team} height="30px" /></td>
-                                                <td>{team.team}</td>
-                                                <td>{team.goalDifference}</td> 
-                                            </tr>
-                                        </tbody>
+                                        <tr key={team.teamId}>
+                                            <td><Logo src={team.crestURI} alt={team.team} height="30px" /></td>
+                                            <td>{team.team}</td>
+                                            <td>{team.goalDifference}</td>
+                                            <td>{team.points}</td>
+                                        </tr>
                                     )
-                            })}
+                                })}
+                            </tbody>
                         </table>
                     </div>
                 )
